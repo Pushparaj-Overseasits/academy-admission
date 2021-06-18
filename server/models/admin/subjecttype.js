43df3ef809074771851f0b7857ab9b894081dbc3
Monika Yadav <monika.yadav@overseasits.com>
2021-06-18 14:24:05 +0530

    Subjectcollection add

---
+++ server/models/admin/subjecttype.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const SubjectTypeModel = new mongoose.Schema({
+  id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  clgId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'tblCollege',
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  scode: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('tblSubjecttype', SubjectTypeModel);
